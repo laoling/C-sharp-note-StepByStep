00e307b6f1a0c771cb5bce841bea5e1e0e091c08
老零 <838618014@qq.com>
2015-06-15 16:32:54 +0800

    add first console app project to index Projects

---
+++ Projects/ConsoleExampleApplication1/ConsoleExampleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExampleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //这里我们用来输出一段文字
+            Console.WriteLine("This is my first app of C#!Hello,C#!");
+            Console.ReadKey();
+        }
+    }
+}
