9c4a096f240974f79a864d3356a49044054d3bd0
老零 <838618014@qq.com>
2015-06-23 11:26:28 +0800

    add two example projects

---
+++ Projects/Chapter05Example02/Chapter05Example02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter05Example02
+{
+    //这个是枚举的实例
+    enum orientation : byte
+    {
+        //定义变量并赋值
+        north = 1,
+        south = 2,
+        east  = 3,
+        west  = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /* orientation myDirection = orientation.north;
+            Console.WriteLine("My Direction is {0}.", myDirection);
+            Console.ReadKey(); 
+          */
+            //输出为“My Direction is north.”
+            //下面我们转换为其他类型看看输出结果如何：
+            byte directionByte;
+            string directionString;
+
+            orientation myDirection = orientation.north;
+            Console.WriteLine("My Direction is {0}.", myDirection);
+
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            /* 与上面转换结果相同的一种写法：
+            directionString = myDirection.ToString();
+             */
+
+            Console.WriteLine("Byte 输出为： {0}", directionByte);
+            Console.WriteLine("String 输出为： {0}", directionString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/Chapter05Example03/Chapter05Example03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter05Example03
+{
+    //本实例用于演示结构变量类型
+    //这里的代码由例二扩展所得
+
+    enum orientation : byte
+    { 
+        north = 1,
+        south = 2,
+        east  = 3,
+        west  = 4
+    }
+
+    struct route
+    {
+        public orientation direction;
+        public double      distance;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            route myRoute;
+            int myDirection = -1;
+            double myDistance;
+            Console.WriteLine("1) 北\n2) 南\n3) 东\n4) 西");
+
+            do
+            {
+                Console.WriteLine("从上面选项中选择一个方向：");
+                myDirection = Convert.ToInt32(Console.ReadLine());           
+            } while ((myDirection < 1) || (myDirection > 4));
+            Console.WriteLine("请输入一个距离：（公里数）");
+            myDistance = Convert.ToDouble(Console.ReadLine());
+
+            myRoute.direction = (orientation)myDirection;
+            myRoute.distance = myDistance;
+            Console.WriteLine("我的路线指定的方向是 {0}，指定的距离是 {1}!", myRoute.direction, myRoute.distance);
+
+            Console.ReadKey();
+
+        }
+    }
+}
