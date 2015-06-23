ba888cb68e4c1531f2cd4e411376f5f48077009b
老零 <838618014@qq.com>
2015-06-23 17:26:18 +0800

    add a new example of chapter 5

---
+++ Projects/Chapter05Example04/Chapter05Example04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter05Example04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //本实例用于数组的使用
+            string[] friendNames = {"Robert Barwell", "Mike Parry", "Jeremy Beacock"};
+            Console.WriteLine("这里有我的{0}位好朋友！", friendNames.Length);
+
+            int i;
+            for (i = 0; i < friendNames.Length; i++)
+            {
+                Console.WriteLine(friendNames[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
