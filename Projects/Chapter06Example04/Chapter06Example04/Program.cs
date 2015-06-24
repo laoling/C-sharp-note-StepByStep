5ff825bf3e5631331d9051c1b8c2c9a8d367554e
老零 <838618014@qq.com>
2015-06-24 17:43:05 +0800

    add one new example project of chapter6

---
+++ Projects/Chapter06Example04/Chapter06Example04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter06Example04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //本实例用于命令行参数的展示
+            Console.WriteLine("{0} command line arguments were specified:", args.Length);
+            foreach (string arg in args)
+                Console.WriteLine(arg);
+            Console.ReadKey();
+        }
+    }
+}
