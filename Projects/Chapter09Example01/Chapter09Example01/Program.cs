748bedd3ac38b1dca3e4d36635ecb156c498f292
老零 <838618014@qq.com>
2015-07-01 09:23:13 +0800

    add a new example project of chapter9

---
+++ Projects/Chapter09Example01/Chapter09Example01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter09Example01
+{
+    public abstract class MyBase
+    {
+    }
+
+    internal class MyClass : MyBase
+    {
+    }
+
+    public interface IMyBaseInterface
+    {
+    }
+
+    internal interface IMyBaseInterface2
+    {
+    }
+
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2
+    {
+    }
+
+    internal sealed class MyComplexClass : MyClass, IMyInterface
+    { 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //本实例用于定义类
+            MyComplexClass myObj = new MyComplexClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
