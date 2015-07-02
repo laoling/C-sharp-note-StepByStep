55ee1c40d8214111c014f26320e8248a93fd0959
老零 <838618014@qq.com>
2015-07-02 17:40:16 +0800

    add 2 examples projects of chapter9

---
+++ Projects/Chapter09Example02/Chapter09Example02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chapter09ClassLib;
+
+namespace Chapter09Example02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用类库
+            MyExternalClass myObj = new MyExternalClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/Chapter09Example03/Chapter09Example03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter09Example03
+{
+    class MyClass
+    {
+        public int val;
+    }
+
+    struct myStruct
+    {
+        public int val;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //类和结构
+            MyClass objectA = new MyClass();
+            MyClass objectB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+
+            Console.WriteLine("objectA.val = {0}", objectA.val);
+            Console.WriteLine("objectB.val = {0}", objectB.val);
+            Console.WriteLine("structA.val = {0}", structA.val);
+            Console.WriteLine("structB.val = {0}", structB.val);
+            Console.ReadKey();
+        }
+    }
+}
