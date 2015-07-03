275db43eae0280f3074ff7844711fcd55e596bf1
老零 <838618014@qq.com>
2015-07-03 16:50:01 +0800

    add one example project of Chapter10

---
+++ Projects/Chapter10Example01/Chapter10Example01/MyClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter10Example01
+{
+    public class MyClass
+    {
+        public readonly string Name;
+        private int intVal;
+
+        public int Val
+        {
+            get
+            {
+                return intVal;
+            }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    intVal = value;
+                else
+                    throw (new ArgumentOutOfRangeException("Val", value, "Val must be assigned a value between 0 and 10."));
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name : " + Name + "\nVal : " + Val;
+        }
+
+        private MyClass() : this("Default Name")
+        { 
+        }
+        public MyClass(string newName)
+        {
+            Name = newName;
+            intVal = 0;
+        }
+    }
+}
---
+++ Projects/Chapter10Example01/Chapter10Example01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter10Example01
+{
+    //本实例演示使用方法、属性和字段
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating object myObj...");
+            MyClass myObj = new MyClass("My Object");
+            Console.WriteLine("Object created.");
+
+            for (int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    Console.WriteLine("\nAttempting to assign {0} to myObj.Val...", i);
+                    myObj.Val = i;
+                    Console.WriteLine("Value {0} assigned to myObj.Val.", myObj.Val);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception {0}thrown.", e.GetType().FullName);
+                    Console.WriteLine("Message:\n\"{0}\"", e.Message);
+                }
+            }
+
+            Console.WriteLine("\nOutputing myObj.ToString()...");
+            Console.WriteLine(myObj.ToString());
+            Console.WriteLine("myObj.ToString() output.");
+            Console.ReadKey();
+        }
+    }
+}
