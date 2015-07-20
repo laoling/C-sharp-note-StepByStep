20f2f1ca8df841abcf1d3ee280a363115b8c26ce
老零 <838618014@qq.com>
2015-07-20 17:41:40 +0800

    add a new example of chapter11

---
+++ Projects/Chapter11Example03/Chapter11Example03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter11Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //本实例用于演示一个获取素数的迭代器
+            Primes PrimesFrom2To1000 = new Primes(2, 1000);
+
+            foreach (long i in PrimesFrom2To1000)
+                Console.Write("{0} ", i);
+            Console.ReadKey();
+        }
+    }
+}
