6c880c7b5ee4a76c6a29fccc277a9f3962841c47
老零 <838618014@qq.com>
2015-07-28 14:17:34 +0800

    add an example project of Chapter12

---
+++ Projects/Chapter12Example01/Chapter12Example01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//本实例用于介绍可空类型Vector
+namespace Chapter12Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = GetVector("vector1");
+            Vector v2 = GetVector("vector1");
+            Console.WriteLine("{0} + {1} = {2}", v1, v2, v1 + v2);
+            Console.WriteLine("{0} - {1} = {2}", v1, v2, v1 - v2);
+            Console.ReadKey();
+        }
+
+        static Vector GetVector(string name)
+        {
+            Console.WriteLine("Input {0} magnitude:", name);
+            double? r = GetNullableDouble();
+            Console.WriteLine("Input {0} angle (in degrees):", name);
+            double? theta = GetNullableDouble();
+            return new Vector(r, theta);
+        }
+
+        static double? GetNullableDouble()
+        {
+            double? result;
+            string userInput = Console.ReadLine();
+            try
+            {
+                result = double.Parse(userInput);
+            }
+            catch
+            {
+                result = null;
+            }
+            return result;
+        }
+    }
+}
