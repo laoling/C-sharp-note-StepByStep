1757966fc55c64e2d8f4f344ea9fd96f13b151d7
老零 <838618014@qq.com>
2015-07-28 17:16:29 +0800

    add an example of Chapter12

---
+++ Projects/Chapter12Example02/Chapter12Example02/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter12Example02
+{
+    public abstract class Animal
+    {
+        protected string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Animal()
+        {
+            name = "This animal with no name";
+        }
+
+        public Animal(string NewName)
+        {
+            name = NewName;
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine("{0} has been fed.", name);
+        }
+    }
+}
---
+++ Projects/Chapter12Example02/Chapter12Example02/Chicken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter12Example02
+{
+    class Chicken : Animal 
+    {
+        public void LayEgg()
+        {
+            Console.WriteLine("{0} has layed an egg.", name);
+        }
+
+        public Chicken(String NewName) : base(NewName)
+        { 
+            //do something...
+        }
+    }
+}
