5db7886fa5f423f528828da41000c5e207878051
老零 <838618014@qq.com>
2015-07-31 17:01:08 +0800

    add an example project of Chapter12

---
+++ Projects/Chapter12Example04/Chapter12Example04/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter12Example04
+{
+    public abstract class Animal
+    {
+        protected string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Animal()
+        {
+            name = "This animal with no name";
+        }
+
+        public Animal(string NewName)
+        {
+            name = NewName;
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine("{0} has been fed.", name);
+        }
+
+        public abstract void MakeANoise();
+    }
+}
