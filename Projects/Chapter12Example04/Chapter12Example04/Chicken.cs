5aab3c2be3c9b52058b4c67744e73706288bea18
老零 <838618014@qq.com>
2015-07-31 16:49:51 +0800

    add an example project of Chapter12

---
+++ Projects/Chapter12Example04/Chapter12Example04/Chicken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter12Example04
+{
+    class Chicken : Animal 
+    {
+        public void LayEgg()
+        {
+            Console.WriteLine("{0} has layed an egg.", name);
+        }
+
+        public Chicken(String NewName) : base(NewName)
+        { 
+            //do something...
+        }
+
+        public override void MakeANoise()
+        {
+            Console.WriteLine("{0} says Cluck!", name);
+        }
+        
+    }
+}
