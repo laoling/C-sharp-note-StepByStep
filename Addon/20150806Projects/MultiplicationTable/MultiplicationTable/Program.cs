05c880fd5e16967821a581cbad7f69dd64eeeaba
老零 <838618014@qq.com>
2015-08-06 16:51:36 +0800

    add an simple example in Addon

---
+++ Addon/20150806Projects/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //本实例使用for循环输出九九乘法表
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + "*" + i + "=" + j * i + "\t");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
