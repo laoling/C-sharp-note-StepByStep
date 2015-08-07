f71d04c2add8ad46dddc68278b6ae5d28b8ebb60
老零 <838618014@qq.com>
2015-08-07 16:32:15 +0800

    add a new example project of Chapter13

---
+++ Projects/Chapter13Example01/Chapter13Example01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace Chapter13Example01
+{
+    class Program
+    {
+        //本实例用于简单的处理事件
+        static int counter = 0;
+
+        static string displayString = "This string will appear one letter at a time.";
+
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(100);
+            myTimer.Elapsed += new ElapsedEventHandler(WriteChar);
+            myTimer.Start();
+            Console.ReadKey();
+        }
+
+        static void WriteChar(object source, ElapsedEventArgs e)
+        {
+            Console.Write(displayString[counter++ % displayString.Length]);
+        }
+    }
+}
