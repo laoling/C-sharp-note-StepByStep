13b7b073b06c426b9e63ebc03efecac42b869d53
老零 <838618014@qq.com>
2015-08-10 16:36:12 +0800

    add an example project of Chapter14

---
+++ Projects/Chapter14Example01/Chapter14Example01/Cow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter14Example01
+{
+    public class Cow : Animal
+    {
+        public void Milk()
+        {
+            Console.WriteLine("{0} has been milked.", name);
+        }
+
+        //public Cow(String NewName) : base(NewName)
+        //{ 
+        //    //do something...
+        //}
+
+        public override void MakeANoise()
+        {
+            Console.WriteLine("{0} says Moo!", name);
+        }
+    }
+}
---
+++ Projects/Chapter14Example01/Chapter14Example01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter14Example01
+{
+    //本实例用于使用初始化器
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farm<Animal> farm = new Farm<Animal> 
+            { 
+                //new Cow { Name = "Norris"},
+                //new Chicken { Name = "Rita"},
+                //new Chicken(),
+                //new SuperCow { Name = "Chesney"}
+
+                Animals =
+                {
+                    new Cow { Name = "Norris"},
+                    new Chicken { Name = "Rita"},
+                    new Chicken(),
+                    new SuperCow { Name = "Chesney"}
+                }
+            };
+
+            farm.MakeNoises();
+            Console.ReadKey();
+        }
+    }
+}
