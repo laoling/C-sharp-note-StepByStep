7e83bac948b7f79393a87db9c06b164234e7dee1
老零 <838618014@qq.com>
2015-08-14 16:47:48 +0800

    add an example project of Chapter14

---
+++ Projects/Chapter14Example05/Chapter14Example05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chapter14ExtensionLib;
+
+namespace Chapter14Example05
+{
+    //本实例用于演示定义和使用扩展方法
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Emter a string to convert:");
+            string sourceString = Console.ReadLine();
+            Console.WriteLine("String with title casing: {0}", sourceString.GetWords(capitalizeWords: true).AsSentence());
+            Console.WriteLine("String backwards: {0}", sourceString.GetWords(reverseOrder: true, reverseWords: true).AsSentence());
+            Console.WriteLine("String length backwards: {0}", sourceString.Length.ToStringReversed());
+
+            Console.ReadKey();
+        }
+    }
+}
