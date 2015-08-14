e4a0848e2f8d95cf047a21c3fdc6c2516b8bc31c
老零 <838618014@qq.com>
2015-08-14 14:42:07 +0800

    add an example project of Chapter14

---
+++ Projects/Chapter14Example04/Chapter14Example04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter14Example04
+{
+    //本实例用于演示命名参数和可选参数的使用
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "thahdai adjudn, this isn andml jdaojaoj p!";
+            List<string> words;
+
+            words = WordProcessor.GetWords(sentence);
+            Console.WriteLine("Original sentence:");
+            foreach (string word in words)
+            {
+                Console.Write(word);
+                Console.Write(' ');
+            }
+            Console.WriteLine("\n");
+
+            words = WordProcessor.GetWords(
+                sentence,
+                reverseWords: true,
+                capitalizeWords: true);
+            Console.WriteLine("Capitalized sentence with reversed words:");
+            foreach (string word in words)
+            {
+                Console.Write(word);
+                Console.Write(' ');
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
