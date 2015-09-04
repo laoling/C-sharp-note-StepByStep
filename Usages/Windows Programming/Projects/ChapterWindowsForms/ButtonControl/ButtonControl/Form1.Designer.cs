42c113bd8a174228af5b71e019ee6c15eca4d764
老零 <838618014@qq.com>
2015-09-04 10:01:52 +0800

    add an example project in BottonControl

---
+++ Usages/Windows Programming/Projects/ChapterWindowsForms/ButtonControl/ButtonControl/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ButtonControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEnglish = new System.Windows.Forms.Button();
+            this.buttonChinese = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonEnglish
+            // 
+            this.buttonEnglish.Location = new System.Drawing.Point(20, 23);
+            this.buttonEnglish.Name = "buttonEnglish";
+            this.buttonEnglish.Size = new System.Drawing.Size(115, 27);
+            this.buttonEnglish.TabIndex = 0;
+            this.buttonEnglish.Text = "English";
+            this.buttonEnglish.UseVisualStyleBackColor = true;
+            this.buttonEnglish.Click += new System.EventHandler(this.buttonEnglish_Click);
+            // 
+            // buttonChinese
+            // 
+            this.buttonChinese.Location = new System.Drawing.Point(189, 23);
+            this.buttonChinese.Name = "buttonChinese";
+            this.buttonChinese.Size = new System.Drawing.Size(115, 27);
+            this.buttonChinese.TabIndex = 1;
+            this.buttonChinese.Text = "Chinese";
+            this.buttonChinese.UseVisualStyleBackColor = true;
+            this.buttonChinese.Click += new System.EventHandler(this.buttonChinese_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(107, 56);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(115, 27);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 88);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonChinese);
+            this.Controls.Add(this.buttonEnglish);
+            this.Name = "Form1";
+            this.Text = "Do you speak English?";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnglish;
+        private System.Windows.Forms.Button buttonChinese;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
+
---
+++ Usages/Windows Programming/Projects/ChapterWindowsForms/ButtonControl/ButtonControl/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ButtonControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnglish_Click(object sender, EventArgs e)
+        {
+            Text = "Do you speak English?";
+        }
+
+        private void buttonChinese_Click(object sender, EventArgs e)
+        {
+            Text = "你会说中国话吗？";
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
