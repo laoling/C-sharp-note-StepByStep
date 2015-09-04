6ede6049db12937d75c8055f47038da94666028d
老零 <838618014@qq.com>
2015-09-04 16:01:59 +0800

    add an example project in TextBoxControl

---
+++ Usages/Windows Programming/Projects/ChapterWindowsForms/TextBoxControl/TextBoxControl/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace TextBoxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.labelOccupation = new System.Windows.Forms.Label();
+            this.textBoxOccupation = new System.Windows.Forms.TextBox();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(36, 30);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(41, 12);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "姓名：";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(37, 67);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(41, 12);
+            this.labelAddress.TabIndex = 1;
+            this.labelAddress.Text = "地址：";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(144, 29);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(311, 21);
+            this.textBoxName.TabIndex = 2;
+            this.textBoxName.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxEmpty_Validating);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.Location = new System.Drawing.Point(144, 67);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxAddress.Size = new System.Drawing.Size(311, 99);
+            this.textBoxAddress.TabIndex = 3;
+            this.textBoxAddress.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxEmpty_Validating);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(485, 29);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(80, 20);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHelp.CausesValidation = false;
+            this.buttonHelp.Location = new System.Drawing.Point(485, 68);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(80, 20);
+            this.buttonHelp.TabIndex = 5;
+            this.buttonHelp.Text = "帮助";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // labelOccupation
+            // 
+            this.labelOccupation.AutoSize = true;
+            this.labelOccupation.Location = new System.Drawing.Point(36, 186);
+            this.labelOccupation.Name = "labelOccupation";
+            this.labelOccupation.Size = new System.Drawing.Size(71, 12);
+            this.labelOccupation.TabIndex = 6;
+            this.labelOccupation.Text = "职业（P）：";
+            // 
+            // textBoxOccupation
+            // 
+            this.textBoxOccupation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOccupation.Location = new System.Drawing.Point(144, 186);
+            this.textBoxOccupation.Name = "textBoxOccupation";
+            this.textBoxOccupation.Size = new System.Drawing.Size(311, 21);
+            this.textBoxOccupation.TabIndex = 7;
+            this.textBoxOccupation.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxOccupation_Validating);
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(36, 223);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(41, 12);
+            this.labelAge.TabIndex = 8;
+            this.labelAge.Text = "年龄：";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAge.Location = new System.Drawing.Point(144, 223);
+            this.textBoxAge.MaxLength = 3;
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(311, 21);
+            this.textBoxAge.TabIndex = 9;
+            this.textBoxAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxAge_KeyPress);
+            this.textBoxAge.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxEmpty_Validating);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(36, 255);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(65, 12);
+            this.labelOutput.TabIndex = 10;
+            this.labelOutput.Text = "输出结果：";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(39, 279);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(415, 168);
+            this.textBoxOutput.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 463);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.textBoxOccupation);
+            this.Controls.Add(this.labelOccupation);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelName);
+            this.MinimumSize = new System.Drawing.Size(612, 501);
+            this.Name = "Form1";
+            this.Text = "TextBoxControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Label labelOccupation;
+        private System.Windows.Forms.TextBox textBoxOccupation;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+    }
+}
+
---
+++ Usages/Windows Programming/Projects/ChapterWindowsForms/TextBoxControl/TextBoxControl/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TextBoxControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //按钮属性设置为false，这样输入完信息之前按钮都不会生效
+            buttonOK.Enabled = false;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            //定义输入字符串
+            string output;
+
+            //获取四个TextBoxes的Value值
+            output = "Name: " + this.textBoxName.Text + "\r\n";
+            output += "Address: " + this.textBoxAddress.Text + "\r\n";
+            output += "Occupation: " + this.textBoxOccupation.Text + "\r\n";
+            output += "Age: " + this.textBoxAge.Text + "\r\n";
+
+            //将output输出到text属性中
+            this.textBoxOutput.Text = output;
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            //定义输入字符串，这里我们将一个简短描述输出到output中
+            string output;
+
+            //这里写四行简短描述，描述各栏的用途
+            output = "Name: 输入名字\r\n";
+            output += "Address: 输入地址\r\n";
+            output += "Occupation: 这里只能输入‘程序员’\r\n";
+            output += "Age: 输入年龄\r\n";
+
+            this.textBoxOutput.Text = output;
+        }
+
+        private void textBoxEmpty_Validating(object sender, CancelEventArgs e)
+        {
+            //因为有多个文本框使用了这个方法来处理事件，所以我们不知道那个控件调用了函数，
+            //但无论哪个使用，其结果是一样的，所以对传递给文本框的sender参数进行转换。
+            TextBox tb = (TextBox)sender;
+
+            //如果文本框为空，文本框背景色设为红色；否则保持系统设置的颜色
+            if (tb.Text.Length == 0)
+                tb.BackColor = Color.Red;
+            else
+                tb.BackColor = System.Drawing.SystemColors.Window;
+
+            ValidateOK();
+        }
+
+        private void textBoxAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //0~9的ASCII值是48~57，8是退格键
+            //Handled属性设置为true，表示按键时对此动作跳过，不做任何处理
+            //达到的效果：只能输入删除数字
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void textBoxOccupation_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            //检查填入的是否程序员
+            if (tb.Text == "程序员")
+                tb.BackColor = System.Drawing.SystemColors.Window;
+            else
+                tb.BackColor = Color.Red;
+
+            ValidateOK();
+        }
+
+        private void ValidateOK()
+        { 
+            this.buttonOK.Enabled = (textBoxName.BackColor != Color.Red &&
+                                     textBoxAddress.BackColor != Color.Red &&
+                                     textBoxAge.BackColor != Color.Red &&
+                                     textBoxOccupation.BackColor != Color.Red);
+        }
+    }
+}
