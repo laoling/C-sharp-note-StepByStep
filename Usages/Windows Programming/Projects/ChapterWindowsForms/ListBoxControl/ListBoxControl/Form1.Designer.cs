49ffb48036e08a8956a4d20135a55b0b2258d666
老零 <838618014@qq.com>
2015-09-08 16:00:10 +0800

    add an example project in ListBoxControl

---
+++ Usages/Windows Programming/Projects/ChapterWindowsForms/ListBoxControl/ListBoxControl/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ListBoxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxPossibleValues = new System.Windows.Forms.CheckedListBox();
+            this.buttonMove = new System.Windows.Forms.Button();
+            this.listBoxSelected = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxPossibleValues
+            // 
+            this.checkedListBoxPossibleValues.CheckOnClick = true;
+            this.checkedListBoxPossibleValues.FormattingEnabled = true;
+            this.checkedListBoxPossibleValues.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine"});
+            this.checkedListBoxPossibleValues.Location = new System.Drawing.Point(27, 19);
+            this.checkedListBoxPossibleValues.Name = "checkedListBoxPossibleValues";
+            this.checkedListBoxPossibleValues.Size = new System.Drawing.Size(174, 228);
+            this.checkedListBoxPossibleValues.TabIndex = 0;
+            // 
+            // buttonMove
+            // 
+            this.buttonMove.Location = new System.Drawing.Point(221, 135);
+            this.buttonMove.Name = "buttonMove";
+            this.buttonMove.Size = new System.Drawing.Size(83, 22);
+            this.buttonMove.TabIndex = 1;
+            this.buttonMove.Text = "Move";
+            this.buttonMove.UseVisualStyleBackColor = true;
+            this.buttonMove.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // listBoxSelected
+            // 
+            this.listBoxSelected.FormattingEnabled = true;
+            this.listBoxSelected.ItemHeight = 12;
+            this.listBoxSelected.Location = new System.Drawing.Point(318, 24);
+            this.listBoxSelected.Name = "listBoxSelected";
+            this.listBoxSelected.Size = new System.Drawing.Size(154, 220);
+            this.listBoxSelected.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 262);
+            this.Controls.Add(this.listBoxSelected);
+            this.Controls.Add(this.buttonMove);
+            this.Controls.Add(this.checkedListBoxPossibleValues);
+            this.Name = "Form1";
+            this.Text = "Lists";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxPossibleValues;
+        private System.Windows.Forms.Button buttonMove;
+        private System.Windows.Forms.ListBox listBoxSelected;
+    }
+}
+
---
+++ Usages/Windows Programming/Projects/ChapterWindowsForms/ListBoxControl/ListBoxControl/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ListBoxControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkedListBoxPossibleValues.Items.Add("Ten");
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            if (checkedListBoxPossibleValues.CheckedItems.Count > 0)
+            {
+                listBoxSelected.Items.Clear();
+                foreach (string item in checkedListBoxPossibleValues.CheckedItems)
+                {
+                    listBoxSelected.Items.Add(item.ToString());
+                }
+                for (int i = 0; i < checkedListBoxPossibleValues.Items.Count; i++)
+                    checkedListBoxPossibleValues.SetItemChecked(i, false);
+            }
+        }
+    }
+}
